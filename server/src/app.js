4013b97bb45079401d29abfcb3905c077a5551c2
Titan007 <sanskargambhir93@gmail.com>
2026-01-15 01:10:44 +0530

    Backend Database

--- server/src/app.js
+++ server/src/app.js
@@ -1,28 +1,30 @@
 import express from "express"
 import cors from "cors"
-// Import Routes
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import voterRouter from "./routes/voters.routes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-// CORS Configuration
 app.use(cors({
-  origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
   credentials:true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Methods that we are supporting
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], 
   allowedHeaders: ["Authorization", "Content-Type"],
 }))
 
-// Basic Configurations
-app.use(express.json({ limit:"16kb" })) // Accept json data
-app.use(express.urlencoded({ extended:true, limit:"16kb" })) // Accept parameters from the url
-app.use(express.static("public")) // Use "public" folder
-app.use(cookieParser()) // Parse cookies
+
+app.use(express.json({ limit:"16kb" })) 
+app.use(express.urlencoded({ extended:true, limit:"16kb" })) 
+app.use(express.static("public")) 
+app.use(cookieParser()) 
+
+console.log("✅ app.js loaded");
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/voters", voterRouter);
 
 app.get('/', (req, res) => {
   res.send("Welcome to my Project")
