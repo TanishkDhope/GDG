1c3c320625f2c109c486b340b1f9d80600c8062a
Titan007 <sanskargambhir93@gmail.com>
2026-01-15 01:45:14 +0530

    Dummy Data

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
 
 app.use(express.json({ limit:"16kb" })) 
 app.use(express.urlencoded({ extended:true, limit:"16kb" })) 
-app.use(express.static("public")) 
+app.use("/images", express.static("public/images"));
 app.use(cookieParser()) 
 
 console.log("✅ app.js loaded");
